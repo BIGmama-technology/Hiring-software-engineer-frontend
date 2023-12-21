fe3327f251ead1ce1b0862d514a54ab305b5d3e7
Bretis2019 <mosbahiwalid10@gmail.com>
2023-12-21 11:25:20 +0100

    fixed some css in dashboard

--- Bigmama/src/app/dashboard/rooms-display.tsx
+++ Bigmama/src/app/dashboard/rooms-display.tsx
@@ -10,6 +10,7 @@ export default function RoomsDisplay({ session }: { session: Session | null }) {
     const [loading, setLoading] = useState(true)
     const [rooms, setRooms] = useState<string[]>([])
     const [title, setTitle] = useState<string>("")
+    const [showmodal, setShowmodal] = useState(false)
     const [joinroomId, setJoinroomId] = useState<string>("")
 
     const user = session?.user
@@ -116,55 +117,134 @@ export default function RoomsDisplay({ session }: { session: Session | null }) {
         let title = parts[0];
         let id = parts[1];
         return (
-            <div>
-                <h1>{title}</h1>
-                <p>{id}</p>
-                <Link href={`/room/?room=${room}`}><div>View Room</div></Link>
+            <div className={"border-2 border-gray-500 bg-white rounded-2xl w-fit px-4 py-2 flex flex-col justify-between h-fit gap-y-2"}>
+                <h1 className={"text-black text-2xl font-semibold"}>{title}</h1>
+                <p className={"text-gray-600"}>id: {id?.length > 0 ? id : "none"}</p>
+                <Link href={`/room/?room=${room}`}><div className={"bg-black px-2 py-1 rounded-full w-fit"}>View Room</div></Link>
             </div>)
     })
 
     return (
-        <div className="form-widget">
-
-            {rooms.length > 0 ? roomsElements : <div>You don't have any rooms</div>}
-
-            <div>
-                <div>
-                    <label htmlFor="title">Room title</label>
-                    <input
-                        id="title"
-                        type="text"
-                        value={title || ''}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
-                <button
-                    className="button primary block"
-                    onClick={() => createRoom({ rooms, title })}
-                    disabled={loading}
-                >
-                    {loading ? 'Loading ...' : 'Create Room'}
-                </button>
+        <div className="form-widget px-8 py-2">
+            <div className={"w-full flex justify-between"}>
+                <div className={"text-4xl font-bold"}>My documents</div>
+                <div className={"px-2 py-1 bg-white text-black rounded-lg text-xl cursor-pointer"} onClick={() => setShowmodal((prevState) => !prevState)}>Add new Document</div>
+            </div>
+            <div className={"flex flex-wrap gap-4"}>
+                {rooms.length > 0 ? roomsElements : <div>You don't have any rooms</div>}
             </div>
 
-            <div>
-                <div>
-                    <label htmlFor="roomid">Room Id</label>
-                    <input
-                        id="roomId"
-                        type="text"
-                        value={joinroomId || ''}
-                        onChange={(e) => setJoinroomId(e.target.value)}
-                    />
+            {showmodal && <div className={"w-fit"}>
+                <div className={"flex flex-col gap-y-2"}>
+                    <div>
+                        <label htmlFor="title">Room title</label>
+                        <input
+                            id="title"
+                            type="text"
+                            value={title || ''}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        className="button primary block"
+                        onClick={() => createRoom({ rooms, title })}
+                        disabled={loading}
+                    >
+                        {loading ? 'Loading ...' : 'Create Room'}
+                    </button>
                 </div>
-                <button
-                    className="button primary block"
-                    onClick={() => joinRoom({ rooms, joinroomId })}
-                    disabled={loading}
-                >
-                    {loading ? 'Loading ...' : 'Join Room'}
-                </button>
-            </div>
+
+                <div className={"flex flex-col gap-y-2"}>
+                    <div>
+                        <label htmlFor="roomid">Room Id</label>
+                        <input
+                            id="roomId"
+                            type="text"
+                            value={joinroomId || ''}
+                            onChange={(e) => setJoinroomId(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        className="button primary block"
+                        onClick={() => joinRoom({ rooms, joinroomId })}
+                        disabled={loading}
+                    >
+                        {loading ? 'Loading ...' : 'Join Room'}
+                    </button>
+                </div>
+            </div>}
         </div>
     )
+}
+
+import React from "react";
+
+function Modal() {
+    const [showModal, setShowModal] = React.useState(false);
+    return (
+        <>
+            <button
+                className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                Open regular modal
+            </button>
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Modal Title
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                        I always felt like I could do anything. That’s the main
+                                        thing people are controlled by! Thoughts- their perception
+                                        of themselves! They're slowed down by their perception of
+                                        themselves. If you're taught you can’t do anything, you
+                                        won’t do anything. I was taught I could do everything.
+                                    </p>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+        </>
+    );
 }
