0ae1e8e7e6298db2dd30f8a45145c61904647dca
Louai Dourov <92324961+Louai-Zokerburg@users.noreply.github.com>
2023-12-30 17:58:30 +0100

    update toolbar-block.tsx

--- src/components/editor/toolbar-block.tsx
+++ src/components/editor/toolbar-block.tsx
@@ -2,11 +2,6 @@ import { Editor } from "@tiptap/react";
 import { Button } from "../ui/button";
 import { FaListUl, FaListOl } from "react-icons/fa";
 
-// be able to create any programing aguage on the world if you watn to do so for real y habibi
-
-// loua is the best prgmmer on the earth 
-
-// weel do i really need to so or not !!
 
 type Props = {
   editor: Editor;
@@ -38,7 +33,3 @@ export function ToolbarBlock({ editor }: Props) {
     </>
   );
 }
-
-// got implemnt a new featuure over here but i dot realy know howw to do so
-// ifi nay onw here knows hwo to add multifactor atuh let him do 
-// cuz i dont really give ahsit and i dont really wanna do it
