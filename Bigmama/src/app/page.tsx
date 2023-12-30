3a59a7d593d4d23cc34e5b89256fddcee69dc4c8
Bretis2019 <mosbahiwalid10@gmail.com>
2023-12-30 22:30:58 +0100

    added animation to login screen and added google login

--- Bigmama/src/app/page.tsx
+++ Bigmama/src/app/page.tsx
@@ -2,16 +2,13 @@ import AuthForm from '@/components/auth-form'
 import "./global.css"
 
 export default function Home() {
+
     return (
-        <div className="row">
-            <div className="col-6">
-                <h1 className="landingTitle">
-                    Real-Time document collaboration platform
-                </h1>
-            </div>
-            <div className="col-6 auth-widget">
-                <AuthForm />
-            </div>
+        <div className="flex flex-col items-center justify-center h-full gap-y-5">
+            <h1 className="landingTitle text-center">
+                Real-Time document collaboration platform
+            </h1>
+            <AuthForm />
         </div>
     )
 }
--- Bigmama/src/components/auth-form.tsx
+++ Bigmama/src/components/auth-form.tsx
@@ -4,6 +4,8 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useLottie } from "Lottie-react";
+import animationData from "@/animation/writing_animation.json"
 
 export default function AuthForm() {
     const supabase = createClientComponentClient()
@@ -23,15 +25,29 @@ export default function AuthForm() {
 
         checkUser();
     }, []);
+
+    const options = {
+        animationData: animationData,
+        loop: true
+    };
+
+    const { View } = useLottie(options);
+
     return (
-        <Auth
-            supabaseClient={supabase}
-            view="magic_link"
-            appearance={{ theme: ThemeSupa }}
-            theme="dark"
-            showLinks={false}
-            providers={["discord"]}
-            redirectTo="http://localhost:3000/api/auth/callback"
-        />
+        <div className={"flex flex-col gap-y-3 items-center"}>
+            <div className={"w-28 h-28 bg-white rounded-full overflow-hidden"}>
+                <div className={"z-50"}>{ View }</div>
+            </div>
+            <Auth
+                supabaseClient={supabase}
+                view="sign_in"
+                appearance={{ theme: ThemeSupa }}
+                theme="dark"
+                showLinks={false}
+                providers={["discord", "google"]}
+                socialLayout={"horizontal"}
+                redirectTo="http://localhost:3000/api/auth/callback"
+            />
+        </div>
     )
 }
